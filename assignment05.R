0aed427e965ae2d826ed3f7dc0276b2a147d2b91
Marian Schmidt <marschmi@umich.edu>
2014-11-07 15:02:40 -0500

    find local cells surrounding focal cells

--- assignment05.R
+++ assignment05.R
@@ -13,6 +13,24 @@ matrix <- createMatrix(conditions, nrows=10, ncols=10)
 runSimulations <- function(matrix, nsim=3000){
   nrow <- sample(1:nrow(matrix), 1)
   ncol <- sample(1:ncol(matrix), 1)
-  matrix[nrow, ncol]
+  index <- matrix[nrow, ncol]
+  cell1 <- matrix[nrow-1,ncol-1]
+  cell2 <- matrix[nrow-1,ncol]
+  cell3 <- matrix[nrow-1,ncol+1]
+  cell4 <- matrix[nrow,ncol-1]
+  cell5<- matrix[nrow,ncol+1]
+  cell6 <- matrix[nrow+1,ncol-1]
+  cell7 <- matrix[nrow+1,ncol]
+  cell8 <- matrix[nrow+1,ncol+1]  
+  local <- c(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8)
+  
+  
+  
+findAround <- function(matrix, index){  
+
+  
+  
+  
+  
 }
 
