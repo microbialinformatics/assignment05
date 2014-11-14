e33b2d169f5c5bc20486e70a0e1a6a79d5c9483d
Marian Schmidt <marschmi@umich.edu>
2014-11-14 16:36:17 -0500

    fixed syntax of else if statements

--- assignment05.R
+++ assignment05.R
@@ -15,87 +15,97 @@ runSimulations <- function(matrix, nsim=3000){
   ncol <- sample(1:ncol(matrix), 1)
   index <- c(nrow, ncol)
   index_value <- matrix[nrow, ncol]  
-  if (nrow!=1 & nrow != 250 & ncol != 1 & ncol 1= 250){ # IF IN MIDDLE
+  if(nrow!=1 & nrow != nrow(matrix) & ncol != 1 & ncol != ncol(matrix)) { # IF IN MIDDLE
     cell1 <- matrix[nrow-1,ncol-1]
     cell2 <- matrix[nrow-1,ncol]
     cell3 <- matrix[nrow-1,ncol+1]
     cell4 <- matrix[nrow,ncol-1]
-    cell5<- matrix[nrow,ncol+1]
-    cell6 <- matrix[nrow+1,ncol-1]
-    cell7 <- matrix[nrow+1,ncol]
-    cell8 <- matrix[nrow+1,ncol+1]  }
-  else if (nrow == 1 & ncol != 1 & ncol != 250){ #only for those in row 1 and NO CORNERS!
-    cell1 <- matrix[nrow(matrix),ncol-1]
-    cell2 <- matrix[nrow(matrix),ncol]
-    cell3 <- matrix[nrow(matrix),ncol+1]
-    cell4 <- matrix[nrow,ncol-1]
-    cell5<- matrix[nrow,ncol+1]
-    cell6 <- matrix[nrow+1,ncol-1]
-    cell7 <- matrix[nrow+1,ncol]
-    cell8 <- matrix[nrow+1,ncol+1]  }
-  else if (nrow == 1 & ncol == 1){ #only for those in row 1 and column 1!!! MUST BE TOP LEFT CORNER!
-    cell1 <- matrix[nrow(matrix),ncol(matrix)] #bottom right corner
-    cell2 <- matrix[nrow(matrix),ncol]
-    cell3 <- matrix[nrow(matrix),ncol+1]
-    cell4 <- matrix[nrow,ncol(matrix)] #wrap!
-    cell5<- matrix[nrow,ncol+1]
-    cell6 <- matrix[nrow+1,ncol(matrix)] #wrap!
-    cell7 <- matrix[nrow+1,ncol]
-    cell8 <- matrix[nrow+1,ncol+1]  }
-  else if (nrow == 1 & ncol == 250){ #only for those in row 1 and column 250!  TOP RIGHT CORNER
-    cell1 <- matrix[nrow(matrix),ncol-1]
-    cell2 <- matrix[nrow(matrix),ncol]
-    cell3 <- matrix[nrow(matrix),1] #WRAP!
-    cell4 <- matrix[nrow,ncol-1]
-    cell5<- matrix[nrow,1]#WRAP!
-    cell6 <- matrix[nrow+1,ncol-1]
-    cell7 <- matrix[nrow+1,ncol]
-    cell8 <- matrix[nrow+1,1]  }
-  else if (nrow == 250 & ncol != 1 & ncol != 250){ # NO CORNERS LAST ROW
-    cell1 <- matrix[nrow-1,ncol-1]
-    cell2 <- matrix[nrow-1,ncol]
-    cell3 <- matrix[nrow-1,ncol+1]
-    cell4 <- matrix[nrow,ncol-1]
-    cell5<- matrix[nrow,ncol+1]
-    cell6 <- matrix[1,ncol-1]
-    cell7 <- matrix[1,ncol]
-    cell8 <- matrix[1,ncol+1] }
-  else if (nrow == 250 & ncol == 1){ # BOTTOM LEFT CORNER
-    cell1 <- matrix[nrow-1,ncol(matrix)] #matrix!
-    cell2 <- matrix[nrow-1,ncol] 
-    cell3 <- matrix[nrow-1,ncol+1]
-    cell4 <- matrix[nrow,ncol(matrix)] #wrap
     cell5 <- matrix[nrow,ncol+1]
-    cell6 <- matrix[1,ncol(matrix)] #kiddy corner !
-    cell7 <- matrix[1,ncol] #wrap
-    cell8 <- matrix[1,ncol+1] #wrap  
-  else if (nrow == 250 & ncol == 250){ # BOTTOM RIGHT CORNER!
-    cell1 <- matrix[nrow-1,ncol-1]
-    cell2 <- matrix[nrow-1,ncol]
-    cell3 <- matrix[nrow-1,1] #WRAP
-    cell4 <- matrix[nrow,ncol-1]
-    cell5<- matrix[nrow,1] #WRAP
-    cell6 <- matrix[1,ncol-1]
-    cell7 <- matrix[1,ncol]
-    cell8 <- matrix[1,1] } #WRAP!
-  else if (ncol == 1 & nrow != 1 & nrow != 250){ #middle column 1
-    cell1 <- matrix[nrow-1,ncol(matrix)]
-    cell2 <- matrix[nrow-1,ncol]
-    cell3 <- matrix[nrow-1,ncol+1]
-    cell4 <- matrix[nrow,ncol(matrix)]
-    cell5<- matrix[nrow,ncol+1]
-    cell6 <- matrix[nrow+1,ncol(matrix)]
-    cell7 <- matrix[nrow+1,ncol]
-    cell8 <- matrix[nrow+1,ncol+1]  }    
-  else if (ncol == 250 & nrow != 1 & nrow != 250){ #middle last column!
-    cell1 <- matrix[nrow-1,ncol-1]
-    cell2 <- matrix[nrow-1,ncol]
-    cell3 <- matrix[nrow-1,1]
-    cell4 <- matrix[nrow,ncol-1]
-    cell5<- matrix[nrow,1]
     cell6 <- matrix[nrow+1,ncol-1]
     cell7 <- matrix[nrow+1,ncol]
-    cell8 <- matrix[nrow+1,1] }  
+    cell8 <- matrix[nrow+1,ncol+1]  
+  } else if(nrow == 1 & ncol != 1 & ncol != ncol(matrix)) { #only for those in row 1 and NO CORNERS!
+      cell1 <- matrix[nrow(matrix),ncol-1]
+      cell2 <- matrix[nrow(matrix),ncol]
+      cell3 <- matrix[nrow(matrix),ncol+1]
+      cell4 <- matrix[nrow,ncol-1]
+      cell5 <- matrix[nrow,ncol+1]
+      cell6 <- matrix[nrow+1,ncol-1]
+      cell7 <- matrix[nrow+1,ncol]
+      cell8 <- matrix[nrow+1,ncol+1]  
+  } else if(nrow == 1 & ncol == 1) { #only for those in row 1 and column 1!!! MUST BE TOP LEFT CORNER!
+      cell1 <- matrix[nrow(matrix),ncol(matrix)] #bottom right corner
+      cell2 <- matrix[nrow(matrix),ncol]
+      cell3 <- matrix[nrow(matrix),ncol+1]
+      cell4 <- matrix[nrow,ncol(matrix)] #wrap!
+      cell5 <- matrix[nrow,ncol+1]
+      cell6 <- matrix[nrow+1,ncol(matrix)] #wrap!
+      cell7 <- matrix[nrow+1,ncol]
+      cell8 <- matrix[nrow+1,ncol+1]  
+  } else if(nrow == 1 & ncol == ncol(matrix)) { #only for those in row 1 and column 250!  TOP RIGHT CORNER
+      cell1 <- matrix[nrow(matrix),ncol-1]
+      cell2 <- matrix[nrow(matrix),ncol]
+      cell3 <- matrix[nrow(matrix),1] #WRAP!
+      cell4 <- matrix[nrow,ncol-1]
+      cell5 <- matrix[nrow,1]#WRAP!
+      cell6 <- matrix[nrow+1,ncol-1]
+      cell7 <- matrix[nrow+1,ncol]
+      cell8 <- matrix[nrow+1,1]  
+  } else if(nrow == nrow(matrix) & ncol != 1 & ncol != ncol(matrix)) { # NO CORNERS LAST ROW
+      cell1 <- matrix[nrow-1,ncol-1]
+      cell2 <- matrix[nrow-1,ncol]
+      cell3 <- matrix[nrow-1,ncol+1]
+      cell4 <- matrix[nrow,ncol-1]
+      cell5 <- matrix[nrow,ncol+1]
+      cell6 <- matrix[1,ncol-1]
+      cell7 <- matrix[1,ncol]
+      cell8 <- matrix[1,ncol+1] 
+  } else if(nrow == nrow(matrix) & ncol == 1) { # BOTTOM LEFT CORNER
+      cell1 <- matrix[nrow-1,ncol(matrix)] #matrix!
+      cell2 <- matrix[nrow-1,ncol] 
+      cell3 <- matrix[nrow-1,ncol+1]
+      cell4 <- matrix[nrow,ncol(matrix)] #wrap
+      cell5 <- matrix[nrow,ncol+1]
+      cell6 <- matrix[1,ncol(matrix)] #kiddy corner !
+      cell7 <- matrix[1,ncol] #wrap
+      cell8 <- matrix[1,ncol+1] #wrap  
+  } else if(nrow == nrow(matrix) & ncol == ncol(matrix)) { # BOTTOM RIGHT CORNER!
+      cell1 <- matrix[nrow-1,ncol-1]
+      cell2 <- matrix[nrow-1,ncol]
+      cell3 <- matrix[nrow-1,1] #WRAP
+      cell4 <- matrix[nrow,ncol-1]
+      cell5 <- matrix[nrow,1] #WRAP
+      cell6 <- matrix[1,ncol-1]
+      cell7 <- matrix[1,ncol]
+      cell8 <- matrix[1,1] } #WRAP!
+ } else if(ncol == 1 & nrow != 1 & nrow != nrow(matrix)) { #middle column 1
+      cell1 <- matrix[nrow-1,ncol(matrix)]
+      cell2 <- matrix[nrow-1,ncol]
+      cell3 <- matrix[nrow-1,ncol+1]
+      cell4 <- matrix[nrow,ncol(matrix)]
+      cell5 <- matrix[nrow,ncol+1]
+      cell6 <- matrix[nrow+1,ncol(matrix)]
+      cell7 <- matrix[nrow+1,ncol]
+      cell8 <- matrix[nrow+1,ncol+1]  
+ } else if(ncol == ncol(matrix) & nrow != 1 & nrow != nrow(matrix)) { #middle last column!      
+      cell1 <- matrix[nrow-1,ncol-1]
+      cell2 <- matrix[nrow-1,ncol]
+      cell3 <- matrix[nrow-1,1]
+      cell4 <- matrix[nrow,ncol-1]
+      cell5 <- matrix[nrow,1]
+      cell6 <- matrix[nrow+1,ncol-1]
+      cell7 <- matrix[nrow+1,ncol]
+      cell8 <- matrix[nrow+1,1] 
+ } else { # just in case:  search in the middle (most probable)
+   cell1 <- matrix[nrow-1,ncol-1]
+   cell2 <- matrix[nrow-1,ncol]
+   cell3 <- matrix[nrow-1,ncol+1]
+   cell4 <- matrix[nrow,ncol-1]
+   cell5 <- matrix[nrow,ncol+1]
+   cell6 <- matrix[nrow+1,ncol-1]
+   cell7 <- matrix[nrow+1,ncol]
+   cell8 <- matrix[nrow+1,ncol+1]  
+ }
   local <- c(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8)   
   
   
