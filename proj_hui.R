6222a734e86042ffd70b75ebec1dbfefa3f9ceae
Hui Zhang <ppaohh@umich.edu>
2014-11-19 23:28:36 -0500

    fix replication

--- proj_hui.R
+++ proj_hui.R
@@ -24,13 +24,13 @@ delta.S <- (1/4+3/4*f.C)
 delta.R <- 10/32
 #update this focal point by probability
 if(value=="E")
-  g.neighbor[position] <- sample(c("C","S","R","E"),1,prob=c(f.C,f.S,f.R,f.E)) 
+  g.neighbor[position] <- sample(c("C","S","R","E"),1,replace=T,prob=c(f.C,f.S,f.R,f.E)) 
 if(value=="S")
-  g.neighbor[position] <- sample(c("S","E"),1,prob=c(1-delta.S,delta.S))
+  g.neighbor[position] <- sample(c("S","E"),1,replace=T,prob=c(1-delta.S,delta.S))
 if(value=="C")
-  g.neighbor[position] <- sample(c("C","E"),1,prob=c(1-delta.C,delta.C))
+  g.neighbor[position] <- sample(c("C","E"),1,replace=T,prob=c(1-delta.C,delta.C))
 if(value=="R")
-  g.neighbor[position] <- sample(c("R","E"),1,prob=c(1-delta.R,delta.R))
+  g.neighbor[position] <- sample(c("R","E"),1,replace=T,prob=c(1-delta.R,delta.R))
 
 lattice <- g.neighbor
 }
@@ -41,59 +41,45 @@ return(lattice)
 
 
 
+#x.y <- matrix(data=NA,nrow=5001,ncol=2,byrow=T)
+#abundance.C <- length(which(lattice=="C"))
+#x.y[1,1] <- 0
+#x.y[1,2] <- log10(abundance.C)
+
+#lattice <- replicate(1,update(lattice))
+#abundance.C <- length(which(lattice=="C"))
+#x.y[2,1] <- 1
+#x.y[2,2] <- log10(abundance.C)
+
+#lattice <- replicate(2,update(lattice))
+#abundance.C <- length(which(lattice=="C"))
+#x.y[3,1] <- 2
+#x.y[3,2] <- log10(abundance.C)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#replicate for n times
-time.rep <- function(n,lattice)
+rep <- function(n,lattice,cell)
 {
-  lattice.rep <- replicate(n,update(lattice))
-  lattice.final <- lattice.rep[,,n] 
-  return(lattice.final)
+  x.y <- matrix(data=NA,nrow=1001,ncol=2,byrow=T)
+  i<-0
+  while(i < n+1)
+  {
+    abundance <- length(which(lattice==cell))
+    x.y[i+1,1] <- i
+    x.y[i+1,2] <- log10(abundance)
+    i <- i+1
+    lattice<-replicate(1,update(lattice))
+  }
+  return(x.y)
 }
 
 
 
-#get x and y value
-x.y <- function(x,lattice,cell)
-{
-  x.y <- matrix(data=NA,nrow=x,ncol=2,byrow=T)
-  i=1
-  while(i < x+1)  
-  {
-    lattice.final <- time.rep(i,lattice)
-    total <- length(lattice.final)
-    n.cell <- length(which(lattice.final==cell))
-    y <- log(n.cell)
-    x.y[i,1] <- i
-    x.y[i,2] <- y
-    i <- i+1
-  }
-  
-  return(x.y)
-} 
-
-
 
 #plot
-x.y.C <- x.y(500,lattice,"C")
-plot(x.y.C,type="l",col="red",xlab="Time",ylab="Log(abundance)",main="Global neighbourhood",xlim=c(0,500))
-x.y.S <- x.y(100,lattice,"S")
+x.y.C <- rep(1000,lattice,"C")
+plot(x.y.C,type="l",col="red",xlab="Time",ylab="Log(abundance)",main="Global neighbourhood",xlim=c(0,1000))
+x.y.S <- rep(1000,lattice,"S")
 points(x.y.S[,1],x.y.S[,2],type="l",col="blue")
-x.y.R <- x.y(500,lattice,"R")
+x.y.R <- rep(1000,lattice,"R")
 points(x.y.R[,1],x.y.R[,2],type="l",col="green")
